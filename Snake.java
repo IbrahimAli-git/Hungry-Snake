2593138826dc01fdfa698e6e20a9d0498ec30a8e
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-04-01 08:16:40 +0100

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,12 +1,18 @@
 package com.codegym.games.snake;
 
+import com.codegym.engine.cell.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Snake {
+public class Snake extends GameObject {
+    private static final String HEAD_SIGN = "\\uD83D\\uDC7E";
+    private static final String BODY_SIGN = "\\u26AB";
     private List<GameObject> snakeParts = new ArrayList<>();
 
+
     public Snake(int x, int y){
+        super(x, y);
         GameObject gameObject1 = new GameObject(x, y);
         GameObject gameObject2 = new GameObject(x+1, y);
         GameObject gameObject3 = new GameObject(x+2, y);
@@ -14,4 +20,15 @@ public class Snake {
         snakeParts.add(gameObject2);
         snakeParts.add(gameObject3);
     }
+
+    public void draw(Game game){
+        for (GameObject gameObject : snakeParts) {
+            if (snakeParts.indexOf(gameObject) == 0){
+                game.setCellValue(gameObject.x, gameObject.y, HEAD_SIGN);
+            } else {
+                game.setCellValue(gameObject.x, gameObject.y, BODY_SIGN);
+            }
+        }
+    }
 }
+
