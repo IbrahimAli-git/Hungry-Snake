8d0dd1266067b0662004ad1c3f6b84f9165fca84
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-04-01 09:09:33 +0100

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -11,26 +11,56 @@ public class Snake extends GameObject {
     public boolean isAlive = true;
     private Direction direction = Direction.LEFT;
 
-    public Snake(int x, int y){
+    public Snake(int x, int y) {
         super(x, y);
         GameObject gameObject1 = new GameObject(x, y);
-        GameObject gameObject2 = new GameObject(x+1, y);
-        GameObject gameObject3 = new GameObject(x+2, y);
+        GameObject gameObject2 = new GameObject(x + 1, y);
+        GameObject gameObject3 = new GameObject(x + 2, y);
         snakeParts.add(gameObject1);
         snakeParts.add(gameObject2);
         snakeParts.add(gameObject3);
     }
 
-    public void draw(Game game){
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void move() {
+        GameObject gameObject = createNewHead();
+
+        if (gameObject.x < 0 || gameObject.x >= SnakeGame.WIDTH || gameObject.y < 0 || gameObject.y >= SnakeGame.HEIGHT){
+            isAlive = false;
+        } else {
+            snakeParts.add(0, gameObject);
+            removeTail();
+        }
+    }
+
+    public GameObject createNewHead(){
+        GameObject newHead = snakeParts.get(0);
+        switch (direction){
+            case UP: return new GameObject(newHead.x, newHead.y-1);
+            case RIGHT: return new GameObject(newHead.x+1, newHead.y);
+            case DOWN: return new GameObject(newHead.x, newHead.y+1);
+            case LEFT: return new GameObject(newHead.x-1, newHead.y);
+        }
+        return newHead;
+    }
+
+    public void removeTail(){
+        snakeParts.remove(snakeParts.size()-1);
+    }
+
+    public void draw(Game game) {
         for (GameObject gameObject : snakeParts) {
-            if (snakeParts.indexOf(gameObject) == 0){
-                if (isAlive){
+            if (snakeParts.indexOf(gameObject) == 0) {
+                if (isAlive) {
                     game.setCellValueEx(gameObject.x, gameObject.y, Color.NONE, HEAD_SIGN, Color.BLACK, 75);
                 } else {
                     game.setCellValueEx(gameObject.x, gameObject.y, Color.NONE, HEAD_SIGN, Color.RED, 75);
                 }
             } else {
-                if (isAlive){
+                if (isAlive) {
                     game.setCellValueEx(gameObject.x, gameObject.y, Color.NONE, BODY_SIGN, Color.BLACK, 75);
                 } else {
                     game.setCellValueEx(gameObject.x, gameObject.y, Color.NONE, BODY_SIGN, Color.RED, 75);
@@ -38,14 +68,4 @@ public class Snake extends GameObject {
             }
         }
     }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    public void move(){
-
-    }
 }
-
-
