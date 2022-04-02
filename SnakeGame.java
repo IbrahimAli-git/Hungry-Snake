5e41e8cb4de8d942a38c9ed5ac4efbb454e403bf
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-04-02 10:18:47 +0100

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -8,6 +8,9 @@ public class SnakeGame extends Game {
     private Snake snake;
     private int turnDelay;
     private Apple apple;
+    private boolean isGameStopped;
+    private static final int GOAL = 28;
+    private int score;
 
     @Override
     public void initialize() {
@@ -18,9 +21,24 @@ public class SnakeGame extends Game {
     private void createGame() {
         snake = new Snake(WIDTH / 2, HEIGHT / 2);
         createNewApple();
+        isGameStopped = false;
+        score = 0;
+        setScore(score);
         drawScene();
         turnDelay = 300;
         setTurnTimer(turnDelay);
+    }
+
+    private void win(){
+        stopTurnTimer();
+        isGameStopped = true;
+        showMessageDialog(Color.NONE, "YOU WIN", Color.RED, 75);
+    }
+
+    private void gameOver(){
+        stopTurnTimer();
+        isGameStopped = true;
+        showMessageDialog(Color.NONE, "GAME OVER", Color.RED, 75);
     }
 
     private void drawScene() {
@@ -34,23 +52,39 @@ public class SnakeGame extends Game {
     }
 
     private void createNewApple(){
-       int newWidth =  getRandomNumber(WIDTH);
-       int newHeight = getRandomNumber(HEIGHT);
+        do {
+            int newWidth = getRandomNumber(WIDTH);
+            int newHeight = getRandomNumber(HEIGHT);
 
-       apple = new Apple(newWidth, newHeight);
+            apple = new Apple(newWidth, newHeight);
+        } while (snake.checkCollision(apple));
     }
 
     @Override
     public void onTurn(int step) {
         snake.move(apple);
         if (!apple.isAlive){
+            score += 5;
+            setScore(score);
+            turnDelay -= 10;
+            setTurnTimer(turnDelay);
             createNewApple();
+        }
+        if (!snake.isAlive){
+            gameOver();
+        }
+
+        if (snake.getLength() > GOAL){
+            win();
         }
         drawScene();
     }
 
     @Override
     public void onKeyPress(Key key) {
+        if (key == Key.SPACE && isGameStopped){
+            createGame();
+        }
         switch (key){
             case LEFT: snake.setDirection(Direction.LEFT); break;
             case RIGHT: snake.setDirection(Direction.RIGHT); break;
